cbcdaf0fc32692b07517d196c2ba54875c5ee336
Smael Nicolas <nico.smael@gmail.com>
2022-09-27 21:29:42 -0300

    images uses examples

--- Front/mobile/App.js
+++ Front/mobile/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Assets from "./constants/assets";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>app examples!</Text>
+			<Image
+				source={Assets.home_dark}
+				resizeMode='contain'
+				style={{ width: 25, height: 25 }}
+			/>
+			<Image
+				source={Assets.bell_dark}
+				resizeMode='contain'
+				style={{ width: 25, height: 25 }}
+			/>
+			<Image
+				source={{ uri: "https://picsum.photos/200/300" }}
+				style={{ width: 200, height: 300 }}
+			/>
+			<StatusBar backgroundColor='red' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
