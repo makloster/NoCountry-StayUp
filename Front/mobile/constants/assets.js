1701f400e603a7cf02c839545c288ee0201bfe80
Smael Nicolas <nico.smael@gmail.com>
2022-09-27 21:29:12 -0300

    export url with required to view in web & expo app

---
+++ Front/mobile/constants/assets.js
@@ -0,0 +1,86 @@
+export default {
+	// ICONS Menu > HOME
+	home_dark: require("../assets/Menu/Home/Home_dark.png"),
+	home_dark_filled: require("../assets/Menu/Home/Home_dark_filled.png"),
+	home_light: require("../assets/Menu/Home/Home_light.png"),
+	home_light_filled: require("../assets/Menu/Home/Home_light_filled.png"),
+
+	// // ICONS Menu > LIKES
+	likes_dark: require("../assets/Menu/Likes/Likes_dark.png"),
+	likes_dark_filled: require("../assets/Menu/Likes/Likes_dark_filled.png"),
+	likes_light: require("../assets/Menu/Likes/Likes_light.png"),
+	likes_light_filled: require("../assets/Menu/Likes/Likes_light_filled.png"),
+
+	// // ICONS Menu > PEOPLE
+	people_dark: require("../assets/Menu/People/People_dark.png"),
+	people_dark_filled: require("../assets/Menu/People/People_dark_filled.png"),
+	people_light: require("../assets/Menu/People/People_light.png"),
+	people_light_filled: require("../assets/Menu/People/People_light_filled.png"),
+
+	// // ICONS Menu > Place
+	place_dark: require("../assets/Menu/Places/Place_dark.png"),
+	place_dark_filled: require("../assets/Menu/Places/Place_dark_filled.png"),
+	place_light: require("../assets/Menu/Places/Place_light.png"),
+	place_light_filled: require("../assets/Menu/Places/Place_light_filled.png"),
+
+	// // ICONS Menu > USER
+	user_dark: require("../assets/Menu/Users/User_dark.png"),
+	user_dark_filled: require("../assets/Menu/Users/User_dark_filled.png"),
+	user_light: require("../assets/Menu/Users/User_light.png"),
+	user_light_filled: require("../assets/Menu/Users/User_light_filled.png"),
+
+	// // LOGO
+	logo_dark: require("../assets/Logo/Logo_dark.png"),
+	logo_light: require("../assets/Logo/Logo_light.png"),
+
+	// // ICONS FAVORITES
+	favorite_dark: require("../assets/Favorites/Favorite_dark.png"),
+	favorite_light: require("../assets/Favorites/Favorite_light.png"),
+	favorite_red_filled: require("../assets/Favorites/Favorite_red_filled.png"),
+
+	// // ICONS MESSAGES
+	message_dark: require("../assets/Message/Message_dark.png"),
+	message_dark_dot: require("../assets/Message/Message_dark_dot.png"),
+	message_dark_filled: require("../assets/Message/Message_dark_filled.png"),
+	message_light: require("../assets/Message/Message_light.png"),
+	message_light_dot: require("../assets/Message/Message_light_dot.png"),
+	message_light_filled: require("../assets/Message/Message_light_filled.png"),
+
+	// // ICONS NOTIFICATION
+	bell_dark: require("../assets/Notification/Bell_dark.png"),
+	bell_dark_dot: require("../assets/Notification/Bell_dark_dot.png"),
+	bell_dark_filled: require("../assets/Notification/Bell_dark_filled.png"),
+	bell_light: require("../assets/Notification/Bell_light.png"),
+	bell_light_dot: require("../assets/Notification/Bell_light_dot.png"),
+	bell_light_filled: require("../assets/Notification/Bell_light_filled.png"),
+
+	// ICONS SEARCH BAR
+	filter: require("../assets/SearchBar/Filter.png"),
+	search: require("../assets/SearchBar/Search.png"),
+
+	// ICONS SERVICES
+	bathdroom_dark: require("../assets/Services/Bathdroom_dark.png"),
+	bathdroom_light: require("../assets/Services/Bathdroom_light.png"),
+	drinks_dark: require("../assets/Services/Drinks_dark.png"),
+	drinks_light: require("../assets/Services/Drinks_light.png"),
+	food_dark: require("../assets/Services/Food_dark.png"),
+	food_light: require("../assets/Services/Food_light.png"),
+	hotwater_dark: require("../assets/Services/Hotwater_dark.png"),
+	hotwater_light: require("../assets/Services/Hotwater_light.png"),
+	market_dark: require("../assets/Services/Market_dark.png"),
+	market_light: require("../assets/Services/Market_light.png"),
+	parking_dark: require("../assets/Services/Parking_dark.png"),
+	parking_light: require("../assets/Services/Parking_light.png"),
+	shower_dark: require("../assets/Services/Shower_dark.png"),
+	shower_light: require("../assets/Services/Shower_light.png"),
+	wifi_dark: require("../assets/Services/Wifi_dark.png"),
+	wifi_light: require("../assets/Services/Wifi_light.png"),
+
+	// ICON RED STAR
+	star_red: require("../assets/Star_filled.png"),
+	// ICON upload
+	upload: require("../assets/Upload.png"),
+	//ICON ARROWS
+	arrow_back: require("../assets/Arrow_back.png"),
+	arrow_back_filled: require("../assets/Arrow_filled.png"),
+};
