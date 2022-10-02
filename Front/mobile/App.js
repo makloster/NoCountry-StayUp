09cec9c6e56cc6e89752f679f09bfa458e18d9dd
Smael Nicolas <nico.smael@gmail.com>
2022-10-02 16:09:14 -0300

    add navigation to the app

--- Front/mobile/App.js
+++ Front/mobile/App.js
@@ -1,33 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { LocalScreen } from "./navigation/screens/LocalScreen/LocalScreen";
-
-const Stack = createNativeStackNavigator();
+import Navigation from "./navigation/Navigation";
 
 export default function App() {
-	return (
-		<>
-			<StatusBar backgroundColor='#ffffff' />
-			<NavigationContainer>
-				<Stack.Navigator>
-					<Stack.Screen
-						name='Local'
-						component={LocalScreen}
-						options={{ headerShown: false }}
-					/>
-				</Stack.Navigator>
-			</NavigationContainer>
-		</>
-	);
+	return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
