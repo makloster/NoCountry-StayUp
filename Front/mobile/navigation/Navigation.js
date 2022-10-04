5ec8df06e6a8245c9eb68c07a1108a258ad2e920
Smael Nicolas <nico.smael@gmail.com>
2022-10-04 15:56:50 -0300

    add status bar

--- Front/mobile/navigation/Navigation.js
+++ Front/mobile/navigation/Navigation.js
@@ -1,10 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
-
+import { StatusBar } from "react-native";
 import { BottomTabs } from "./Tabs/BottomTabs";
 
 export default function Navigation() {
 	return (
 		<NavigationContainer>
+			<StatusBar />
 			<BottomTabs />
 		</NavigationContainer>
 	);
