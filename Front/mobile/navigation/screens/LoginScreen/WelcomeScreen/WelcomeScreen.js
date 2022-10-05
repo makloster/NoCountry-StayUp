4087e313b2ed271b984ae74027edbe665621a890
Smael Nicolas <nico.smael@gmail.com>
2022-10-05 18:22:48 -0300

    welcome screen

---
+++ Front/mobile/navigation/screens/LoginScreen/WelcomeScreen/WelcomeScreen.js
@@ -0,0 +1,54 @@
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import {
+	Dimensions,
+	Image,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { CarouselCustom } from "../../../../components/CarouselCustom/CarouselCustom";
+import assets from "../../../../constants/assets";
+// import { WelcomeScreenStyles } from "./WelcomeScreenStyles";
+import { welcomeStyles } from "./WelcomeScreenStyles";
+
+const arrayImages = [
+	{ image: assets.login1 },
+	{ image: assets.login2 },
+	{ image: assets.login3 },
+	{ image: assets.login4 },
+];
+const widthScreen = Dimensions.get("window").width;
+const heightImage = widthScreen - 121;
+
+export default function WelcomeScreen() {
+	// const welcomeStyles = WelcomeScreenStyles();
+
+	return (
+		<ScrollView style={welcomeStyles.containerBig}>
+			<View style={welcomeStyles.containerScreen}>
+				<Image
+					source={assets.principal_logo_light}
+					resizeMode='contain'
+					style={welcomeStyles.logoIcon}
+				/>
+				<CarouselCustom
+					arrayImages={arrayImages}
+					width={widthScreen}
+					height={350}
+					dotsPosition={-30}
+				/>
+				<TouchableOpacity
+					key={`welcome`}
+					style={welcomeStyles.buttonFindPlaces}>
+					<Text style={welcomeStyles.textButtonFindPlaces}>
+						Encontrar Lugares
+					</Text>
+				</TouchableOpacity>
+			</View>
+		</ScrollView>
+	);
+}
