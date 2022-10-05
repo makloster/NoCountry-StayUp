8657108bc5da00a9443569ba855d488a4e48d983
Smael Nicolas <nico.smael@gmail.com>
2022-10-05 20:36:53 -0300

    create stack navigation for login

---
+++ Front/mobile/navigation/Stacks/StackLogin.js
@@ -0,0 +1,53 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { default as HomeScreen } from "../screens/HomeScreen/HomeScreen";
+import { LoginScreen } from "../screens/LoginScreen/LoginScreen";
+import { RegisterScreen } from "../screens/RegisterScreen/RegisterScreen";
+import { WelcomeScreen } from "../screens/WelcomeScreen/WelcomeScreen";
+import { BottomTabs } from "../Tabs/BottomTabs";
+const LoginStackNavigator = createNativeStackNavigator();
+
+export const StackLogin = () => {
+	return (
+		<LoginStackNavigator.Navigator initialRouteName='WelcomeScreen'>
+			<LoginStackNavigator.Screen
+				name='WelcomeScreen'
+				component={WelcomeScreen}
+				options={{
+					headerShown: false,
+					title: "Welcome",
+					headerTitleAlign: "center",
+				}}
+			/>
+			<LoginStackNavigator.Screen
+				name='LoginScreen'
+				component={LoginScreen}
+				options={{
+					headerBackButtonMenuEnabled: true,
+					headerBackTitleVisible: true,
+					headerTransparent: false,
+					title: "Login",
+					headerTitleAlign: "center",
+				}}
+			/>
+			<LoginStackNavigator.Screen
+				name='RegisterScreen'
+				component={RegisterScreen}
+				options={{
+					headerBackButtonMenuEnabled: true,
+					headerBackTitleVisible: true,
+					headerTransparent: false,
+					title: "Register",
+					headerTitleAlign: "center",
+				}}
+			/>
+			<LoginStackNavigator.Screen
+				name='HomeScreen'
+				component={BottomTabs}
+				options={{
+					headerShadowVisible: false,
+					headerShown: false,
+				}}
+			/>
+		</LoginStackNavigator.Navigator>
+	);
+};
