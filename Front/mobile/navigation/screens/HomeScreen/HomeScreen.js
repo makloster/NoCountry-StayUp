ba5272274d0a6d2f5002dbf9c5c2c1bc44769d6f
Smael Nicolas <nico.smael@gmail.com>
2022-10-05 11:58:56 -0300

    example theme

--- Front/mobile/navigation/screens/HomeScreen/HomeScreen.js
+++ Front/mobile/navigation/screens/HomeScreen/HomeScreen.js
@@ -1,16 +1,20 @@
 import {} from "@react-navigation/native";
-import React from "react";
+import React, { useContext } from "react";
 import { ScrollView, Text, View } from "react-native";
+import { ThemeContext } from "../../../Context/Theme";
 
 export default function HomeScreen() {
+	const { backTheme, textTheme } = useContext(ThemeContext);
+
 	return (
-		<ScrollView style={{ backgroundColor: "limegreen" }}>
+		<ScrollView style={{ backgroundColor: backTheme }}>
 			<View>
 				<Text
 					style={{
 						fontSize: 30,
 						textAlign: "center",
 						marginTop: "20%",
+						color: textTheme,
 					}}>
 					HomeScreen
 				</Text>
