3df9c9de146a42e3f7779de6f3ec0acbf191f23f
Smael Nicolas <nico.smael@gmail.com>
2022-10-07 13:18:27 -0300

    theme, componentization

--- Front/mobile/navigation/screens/WelcomeScreen/WelcomeScreenStyles.js
+++ Front/mobile/navigation/screens/WelcomeScreen/WelcomeScreenStyles.js
@@ -1,60 +1,27 @@
-import React from "react";
-import { Dimensions, StyleSheet } from "react-native";
+import { useContext } from "react";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { ThemeContext } from "../../../Context/Theme";
 
-const heightScreen = Dimensions.get("window").height - 100;
+export const WelcomeScreenStyles = () => {
+	const { width, height } = useWindowDimensions();
+	const { backTheme } = useContext(ThemeContext);
 
-export const welcomeStyles = StyleSheet.create({
-	containerBig: {
-		width: "100%",
-		flexDirection: "column",
-		backgroundColor: "black",
-	},
-	containerScreen: {
-		width: "100%",
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "space-between",
-		alignItems: "center",
-		paddingTop: 25,
-	},
-	logoIcon: {
-		width: 250,
-	},
-	buttonFindPlaces: {
-		backgroundColor: "#F6163C",
-		width: 315,
-		height: 65,
-		alignSelf: "center",
-		borderRadius: 15,
-		marginTop: 25,
-	},
-	textButtonFindPlaces: {
-		fontSize: 20,
-		fontWeight: "400",
-		textAlign: "center",
-		color: "white",
-		lineHeight: 65,
-	},
-});
-
-// export const WelcomeScreenStyles = () => {
-// 	return StyleSheet.create({
-// 		containerBig: {
-// 			width: "100%",
-// 			flexDirection: "column",
-// 			backgroundColor: "#181818",
-// 		},
-// 		containerScreen: {
-// 			width: "100%",
-// 			height: heightScreen,
-// 			display: "flex",
-// 			flexDirection: "column",
-// 			justifyContent: "space-between",
-// 			alignItems: "center",
-// 		},
-// 		logoIcon: {
-// 			width: 277,
-// 			height: 78,
-// 		},
-// 	});
-// };
+	return StyleSheet.create({
+		containerBig: {
+			width: width,
+			backgroundColor: backTheme,
+		},
+		containerScreen: {
+			width: width,
+			height: height - 30,
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "center",
+		},
+		logoIcon: {
+			width: 277,
+			height: 78,
+			margin: 25,
+		},
+	});
+};
