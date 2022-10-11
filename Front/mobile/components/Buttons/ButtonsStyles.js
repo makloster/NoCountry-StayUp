8a9ac03321b2ac24ba5c7a3b0ece9e7189dec7f5
Smael Nicolas <nico.smael@gmail.com>
2022-10-11 18:11:48 -0300

    re do styles

--- Front/mobile/components/Buttons/ButtonsStyles.js
+++ Front/mobile/components/Buttons/ButtonsStyles.js
@@ -21,6 +21,21 @@ export const ButtonsStyles = () => {
 			alignItems: "center",
 			marginBottom: 20,
 		},
+		redButtonLogin: {
+			backgroundColor: "#F6163C",
+			width: 300,
+			height: 55,
+			alignSelf: "center",
+			borderRadius: 15,
+			marginTop: 25,
+		},
+		textButtonsLogin: {
+			fontSize: 20,
+			fontWeight: "400",
+			textAlign: "center",
+			color: "white",
+			lineHeight: 55,
+		},
 		imageRegisterOtherWays: {
 			width: 25,
 			height: 25,
