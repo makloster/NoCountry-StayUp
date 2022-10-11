abd2e6790e0aea77089209ccad660518e7c4827f
Smael Nicolas <nico.smael@gmail.com>
2022-10-11 20:35:10 -0300

    check if is under 18

--- Front/mobile/helpers/inputValidations.js
+++ Front/mobile/helpers/inputValidations.js
@@ -21,3 +21,14 @@ export const passwordValidation = (string, setPasswordValid) => {
 export const nameLastNameValidation = (string, setter) => {
 	/^[a-zA-Z]+$/.test(string) ? setter(true) : setter(false);
 };
+
+export const getAge = (dateString) => {
+	const today = new Date();
+	const birthDate = new Date(dateString);
+	const age = today.getFullYear() - birthDate.getFullYear();
+	const m = today.getMonth() - birthDate.getMonth();
+	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+		age--;
+	}
+	return age;
+};
