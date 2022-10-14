ff855a0f8a1d801d311ba2d4914fc77401b1731f
Manuel Kloster <nekrocow@gmail.com>
2022-10-14 13:27:27 -0300

    Update server.js

--- Back/server.js
+++ Back/server.js
@@ -2,7 +2,7 @@ const { app } = require("./app")
 const { connectDB } = require("./config/database")
 const { Models } = require("./models")
 const { relationship } = require('./models/relationship')
-const PORT = process.env.PORT
+const PORT = 4000
 
 const startServer = async () => {
   
