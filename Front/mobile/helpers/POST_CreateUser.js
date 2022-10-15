63af2c8c731f41ef3a2e5f3474ff471c2a2c9424
Smael Nicolas <nico.smael@gmail.com>
2022-10-15 17:13:11 -0300

    fetch post create user

--- Front/mobile/helpers/POST_CreateUser.js
+++ Front/mobile/helpers/POST_CreateUser.js
@@ -1,10 +1,17 @@
-export const POST_CreateUser = () => {
-	fetch("URL", {
+export const POST_CreateUser = (user, setUserCorrect) => {
+	console.log("entra");
+	fetch("http://localhost:4000/api/v1/auth/sign-up", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify(data),
-	});
-	return response.json();
+		body: JSON.stringify(user),
+	})
+		.then((response) => response.json())
+		.then((data) =>
+			data ===
+			"llave duplicada viola restricción de unicidad «users_email_key»"
+				? setUserCorrect(false)
+				: setUserCorrect(true)
+		);
 };
