61a35033843f1f89370911270a08681dc27a8c6c
frangmen <frangm2702@gmail.com>
2022-10-16 21:19:38 -0500

    empezando presentacion 4

--- Front/web/src/components/Home/Presentation3.jsx
+++ Front/web/src/components/Home/Presentation3.jsx
@@ -13,15 +13,15 @@ const Presentation3 = () => {
                     <div className='cards'>
                         <div className='icon_con_texto'>
                             <img src={face_light} alt='face_light' />
-                            <h1 className='texto_en_card'>Hola</h1>
+                            <h1 className='texto_en_card'>Reserva lugares</h1>
                         </div>
                         <div className='icon_con_texto'>
                             <img src={People_light} alt='People_light' />
-                            <h1 className='texto_en_card'>Hola</h1>
+                            <h1 className='texto_en_card'>Conoce Gente</h1>
                         </div>
                         <div className='icon_con_texto'>
                             <img src={Place_light} alt='Place_light' />
-                            <h1 className='texto_en_card'>Hola</h1>
+                            <h1 className='texto_en_card'>Sin preocupaciones</h1>
                         </div>
                     </div>
                 </div>
@@ -31,15 +31,15 @@ const Presentation3 = () => {
                     <div className='cards'>
                         <div className='icon_con_texto'>
                             <img src={face_light} alt='face_light' />
-                            <h1 className='texto_en_card'>Hola</h1>
+                            <h1 className='texto_en_card'>Publica tu actividad</h1>
                         </div>
                         <div className='icon_con_texto'>
                             <img src={People_light} alt='People_light' />
-                            <h1 className='texto_en_card'>Hola</h1>
+                            <h1 className='texto_en_card'>Acepta reservas</h1>
                         </div>
                         <div className='icon_con_texto'>
                             <img src={Place_light} alt='Place_light' />
-                            <h1 className='texto_en_card'>Hola</h1>
+                            <h1 className='texto_en_card'>Gana dinero sin problemas</h1>
                         </div>
                     </div>
                 </div>
---
+++ Front/web/src/components/Home/Presentation4.jsx
@@ -0,0 +1,11 @@
+import './presentation4.css'
+
+import React from 'react'
+
+const Presentation4 = () => {
+  return (
+    <div>Presentation4</div>
+  )
+}
+
+export default Presentation4
