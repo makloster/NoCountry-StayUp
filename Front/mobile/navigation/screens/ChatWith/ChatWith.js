72628a6cf461963478e49553af7f3dbfb1e7056c
Smael Nicolas <nico.smael@gmail.com>
2022-10-18 13:18:52 -0300

    create chat screen

---
+++ Front/mobile/navigation/screens/ChatWith/ChatWith.js
@@ -0,0 +1,100 @@
+import { useNavigation } from "@react-navigation/native";
+import { useContext, useState } from "react";
+import {
+	Image,
+	ScrollView,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import { ButtonChangeTheme } from "../../../components/Buttons/Buttons";
+import assets from "../../../constants/assets";
+import { ThemeContext } from "../../../Context/Theme";
+import { ChatWithStyles } from "./ChatWithStyles";
+
+export const ChatWith = () => {
+	const navigation = useNavigation();
+	const chatWithStyles = ChatWithStyles();
+	const { dark } = useContext(ThemeContext);
+	const [query, setQuery] = useState("");
+	const [message, setMessage] = useState("");
+
+	return (
+		<>
+			<ScrollView style={chatWithStyles.containerBig}>
+				<TouchableOpacity style={chatWithStyles.containerFriend}>
+					<Image
+						source={assets.profile_dummy_7}
+						resizeMode='contain'
+						style={chatWithStyles.friendAvatar}
+					/>
+					<View style={chatWithStyles.containerFriendText}>
+						<Text style={chatWithStyles.friendName}>
+							Raul Rodriguez
+						</Text>
+					</View>
+				</TouchableOpacity>
+				<View style={chatWithStyles.containerMessenger}>
+					<View style={chatWithStyles.containerMessageRecibed}>
+						<Image
+							source={assets.profile_dummy_7}
+							resizeMode='contain'
+							style={chatWithStyles.chatAvatar}
+						/>
+						<Text style={chatWithStyles.textChatRecibed}>
+							Hola! Vi que armaste un grupo nuevo
+						</Text>
+					</View>
+					<View style={chatWithStyles.containerMessageSent}>
+						<Text style={chatWithStyles.textChatSend}>
+							Si, si quieres puedes unirte.
+						</Text>
+
+						<Image
+							source={assets.profile_picture}
+							resizeMode='contain'
+							style={chatWithStyles.chatAvatar}
+						/>
+					</View>
+					<View style={chatWithStyles.containerMessageRecibed}>
+						<Image
+							source={assets.profile_dummy_7}
+							resizeMode='contain'
+							style={chatWithStyles.chatAvatar}
+						/>
+						<Text style={chatWithStyles.textChatRecibed}>
+							Dale genial
+						</Text>
+					</View>
+				</View>
+
+				<ButtonChangeTheme />
+			</ScrollView>
+			<View style={chatWithStyles.containerBoxWriteMessage}>
+				<TouchableOpacity style={chatWithStyles.containerSmileIcon}>
+					<Image
+						source={assets.smile}
+						resizeMode='contain'
+						style={chatWithStyles.iconSmile}
+					/>
+				</TouchableOpacity>
+				<TextInput
+					style={chatWithStyles.chatInput}
+					onChangeText={(e) => setMessage(e)}
+					value={message}
+					keyboardType='default'
+					placeholder='Mensaje'
+					multiline={true}
+				/>
+				<TouchableOpacity>
+					<Image
+						source={assets.mic}
+						resizeMode='contain'
+						style={chatWithStyles.iconsInputMessage}
+					/>
+				</TouchableOpacity>
+			</View>
+		</>
+	);
+};
---
+++ Front/mobile/navigation/screens/ChatWith/ChatWithStyles.js
@@ -0,0 +1,138 @@
+import { useHeaderHeight } from "@react-navigation/elements";
+import { useContext } from "react";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { ThemeContext } from "../../../Context/Theme";
+
+export const ChatWithStyles = () => {
+	const heightHeader = useHeaderHeight();
+	const { width, height } = useWindowDimensions();
+	const { backTheme, textTheme, notBackTheme, notTextTheme } =
+		useContext(ThemeContext);
+
+	return StyleSheet.create({
+		containerBig: {
+			width: width,
+			backgroundColor: backTheme,
+			paddingTop: heightHeader / 2,
+			paddingRight: 15,
+			paddingLeft: 15,
+		},
+		friendAvatar: {
+			width: 51,
+			height: 51,
+			marginRight: 10,
+		},
+		containerFriend: {
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "flex-start",
+			alignItems: "center",
+			marginBottom: 10,
+		},
+		containerFriendText: {
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "flex-start",
+			justifyContent: "flex-start",
+		},
+		friendName: {
+			fontFamily: "ChivoBold",
+			fontSize: 16,
+			color: textTheme,
+		},
+		containerMessenger: {
+			width: width,
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			alignItems: "center",
+		},
+		containerMessageRecibed: {
+			width: "100%",
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "flex-start",
+			alignItems: "center",
+			marginBottom: 20,
+		},
+		containerMessageSent: {
+			width: "80%",
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "flex-end",
+			alignItems: "center",
+			marginBottom: 20,
+		},
+		chatAvatar: {
+			width: 51,
+			height: 51,
+			margin: 5,
+		},
+		textChatRecibed: {
+			fontSize: 15,
+			fontFamily: "ChivoRegular",
+			color: notTextTheme,
+			backgroundColor: notBackTheme,
+			width: 240,
+			height: 65,
+			padding: 10,
+			borderTopRightRadius: 15,
+			borderBottomRightRadius: 15,
+			borderTopLeftRadius: 15,
+		},
+		textChatSend: {
+			fontSize: 15,
+			fontFamily: "ChivoRegular",
+			color: "white",
+			backgroundColor: "#F6163C",
+			width: 240,
+			height: 65,
+			padding: 10,
+			borderTopLeftRadius: 15,
+			borderBottomLeftRadius: 15,
+			borderTopRightRadius: 15,
+		},
+		containerBoxWriteMessage: {
+			alignSelf: "center",
+			width: width,
+			position: "relative",
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "space-between",
+			alignItems: "center",
+			backgroundColor: backTheme,
+			padding: 10,
+		},
+		chatInput: {
+			width: 280,
+			minHeight: 45,
+			color: "#181818",
+			backgroundColor: "white",
+			borderColor: "#181818",
+			borderWidth: 1,
+			borderRadius: 50,
+			fontFamily: "ChivoRegular",
+			paddingLeft: 50,
+		},
+		buttonSendMessageText: {
+			color: "white",
+			fontFamily: "ChivoBold",
+		},
+		iconsInputMessage: {
+			width: 45,
+			height: 45,
+		},
+
+		containerSmileIcon: {
+			position: "absolute",
+			left: 20,
+			top: 18,
+			zIndex: 5,
+			borderRadius: 50,
+		},
+		iconSmile: {
+			width: 29,
+			height: 29,
+		},
+	});
+};
