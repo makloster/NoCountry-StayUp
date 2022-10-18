4e566d9c03ec255c48b72306cc23e8230b4863a8
Smael Nicolas <nico.smael@gmail.com>
2022-10-18 10:24:07 -0300

    notification screens

---
+++ Front/mobile/navigation/screens/NotificationsScreen/NotificationsScreen.js
@@ -0,0 +1,222 @@
+import { useNavigation } from "@react-navigation/native";
+import { useContext } from "react";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ButtonChangeTheme } from "../../../components/Buttons/Buttons";
+import assets from "../../../constants/assets";
+import { ThemeContext } from "../../../Context/Theme";
+import { NotificationsScreenStyles } from "./NotificationsScreenStyles";
+
+export const NotificationsScreen = () => {
+	const navigation = useNavigation();
+	const notificationsScreenStyles = NotificationsScreenStyles();
+	const { dark } = useContext(ThemeContext);
+	return (
+		<ScrollView style={notificationsScreenStyles.containerBig}>
+			<View style={notificationsScreenStyles.containerOptions}>
+				<Text style={notificationsScreenStyles.optionsText}>
+					Notificaciones
+				</Text>
+				<Text style={notificationsScreenStyles.optionsText}>
+					Promociones
+				</Text>
+			</View>
+			<ScrollView horizontal={true}>
+				<View style={notificationsScreenStyles.containerSections}>
+					<Text
+						style={
+							notificationsScreenStyles.lineNotification
+						}></Text>
+					<View
+						style={notificationsScreenStyles.containerNotification}>
+						<View
+							style={
+								notificationsScreenStyles.containerNotificationInfo
+							}>
+							<Image
+								source={assets.group_list_icon}
+								resizeMode='contain'
+								style={notificationsScreenStyles.iconGroup}
+							/>
+							<View
+								style={
+									notificationsScreenStyles.containerNotificationText
+								}>
+								<Text
+									style={
+										notificationsScreenStyles.notificationText
+									}>
+									Se ha completado tu Grupo 130
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationTextBold
+									}>
+									confirma tu pago.
+								</Text>
+							</View>
+						</View>
+						<TouchableOpacity
+							style={
+								notificationsScreenStyles.makeReservationButton
+							}>
+							<Text
+								style={
+									notificationsScreenStyles.notificationButtonsText
+								}>
+								Confirma el pago
+							</Text>
+						</TouchableOpacity>
+					</View>
+					<View
+						style={notificationsScreenStyles.containerNotification}>
+						<View
+							style={
+								notificationsScreenStyles.containerNotificationInfo
+							}>
+							<Image
+								source={assets.profile_dummy_2}
+								resizeMode='contain'
+								style={notificationsScreenStyles.iconGroup}
+							/>
+							<View
+								style={
+									notificationsScreenStyles.containerNotificationText
+								}>
+								<Text
+									style={
+										notificationsScreenStyles.notificationTextBold
+									}>
+									Raul{" "}
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationText
+									}>
+									se ha unido a tu{" "}
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationTextBold
+									}>
+									grupo{" "}
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationText
+									}>
+									de{" "}
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationTextBold
+									}>
+									El rincón -- Cancha de fútbol
+								</Text>
+							</View>
+						</View>
+						<TouchableOpacity
+							style={notificationsScreenStyles.messageToButton}>
+							<Text
+								style={
+									notificationsScreenStyles.messageToButtonText
+								}>
+								Escribele un mensaje
+							</Text>
+						</TouchableOpacity>
+					</View>
+					<View
+						style={notificationsScreenStyles.containerNotification}>
+						<View
+							style={
+								notificationsScreenStyles.containerNotificationInfo
+							}>
+							<Image
+								source={assets.profile_dummy_2}
+								resizeMode='contain'
+								style={notificationsScreenStyles.iconGroup}
+							/>
+							<View
+								style={
+									notificationsScreenStyles.containerNotificationText
+								}>
+								<Text
+									style={
+										notificationsScreenStyles.notificationTextBold
+									}>
+									Raul{" "}
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationText
+									}>
+									esta buscando gente en su{" "}
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationTextBold
+									}>
+									grupo{" "}
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationText
+									}>
+									para{" "}
+								</Text>
+								<Text
+									style={
+										notificationsScreenStyles.notificationTextBold
+									}>
+									El rincón -- Cancha de fútbol
+								</Text>
+							</View>
+						</View>
+						<View
+							style={
+								notificationsScreenStyles.containerNotificationButtons
+							}>
+							<TouchableOpacity
+								style={notificationsScreenStyles.buttonJoin}>
+								<Text
+									style={
+										notificationsScreenStyles.notificationButtonsText
+									}>
+									Unete
+								</Text>
+							</TouchableOpacity>
+							<TouchableOpacity
+								style={
+									notificationsScreenStyles.buttonMessageSmall
+								}>
+								<Text
+									style={
+										notificationsScreenStyles.messageToButtonText
+									}>
+									Mensaje
+								</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</View>
+				<View style={notificationsScreenStyles.containerSections}>
+					<Text
+						style={notificationsScreenStyles.linePromotions}></Text>
+					<TouchableOpacity>
+						<Image
+							source={assets.promo_1}
+							resizeMode='contain'
+							style={notificationsScreenStyles.imagePromotion}
+						/>
+					</TouchableOpacity>
+					<TouchableOpacity>
+						<Image
+							source={assets.promo_2}
+							resizeMode='contain'
+							style={notificationsScreenStyles.imagePromotion}
+						/>
+					</TouchableOpacity>
+				</View>
+			</ScrollView>
+		</ScrollView>
+	);
+};
---
+++ Front/mobile/navigation/screens/NotificationsScreen/NotificationsScreenStyles.js
@@ -0,0 +1,152 @@
+import { useHeaderHeight } from "@react-navigation/elements";
+import { useContext } from "react";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { ThemeContext } from "../../../Context/Theme";
+
+export const NotificationsScreenStyles = () => {
+	const { textTheme, notTextTheme, backTheme, notBackTheme } =
+		useContext(ThemeContext);
+	const { width, height } = useWindowDimensions();
+
+	return StyleSheet.create({
+		containerBig: {
+			width: width,
+			backgroundColor: backTheme,
+			paddingLeft: 15,
+			paddingRight: 15,
+		},
+		containerOptions: {
+			width: 330,
+			height: 40,
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "center",
+			alignItems: "center",
+		},
+		lineNotification: {
+			width: "50%",
+			height: 2,
+			backgroundColor: notBackTheme,
+			alignSelf: "flex-start",
+		},
+		optionsText: {
+			width: "50%",
+			textAlign: "center",
+			fontFamily: "ChivoRegular",
+			fontSize: 16,
+			color: textTheme,
+		},
+		containerSections: {
+			width: 330,
+			minHeight: height,
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "flex-start",
+			alignItems: "center",
+		},
+		containerNotification: {
+			width: 330,
+			height: 110,
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			alignItems: "center",
+		},
+		containerNotificationInfo: {
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "flex-start",
+			alignItems: "center",
+			alignSelf: "flex-start",
+		},
+		containerNotificationText: {
+			width: 240,
+			display: "flex",
+			flexDirection: "row",
+			flexWrap: "wrap",
+			justifyContent: "flex-start",
+			alignItems: "center",
+			color: textTheme,
+		},
+		notificationText: {
+			fontSize: 16,
+			fontFamily: "ChivoRegular",
+			color: textTheme,
+		},
+		notificationTextBold: {
+			fontSize: 16,
+			fontFamily: "ChivoBold",
+			color: textTheme,
+		},
+		notificationButtonsText: {
+			fontSize: 18,
+			fontFamily: "ChivoRegular",
+			color: "white",
+		},
+		iconGroup: {
+			width: 51,
+			height: 51,
+			marginRight: 10,
+		},
+		makeReservationButton: {
+			width: 239,
+			height: 34,
+			marginTop: 5,
+			borderRadius: 20,
+			backgroundColor: "#F6163C",
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+		},
+		messageToButton: {
+			width: 239,
+			height: 34,
+			marginTop: 5,
+			borderRadius: 20,
+			backgroundColor: notBackTheme,
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+		},
+		messageToButtonText: {
+			fontSize: 18,
+			fontFamily: "ChivoRegular",
+			color: notTextTheme,
+		},
+		containerNotificationButtons: {
+			marginTop: 10,
+			display: "flex",
+			flexDirection: "row",
+		},
+		buttonJoin: {
+			width: 113,
+			height: 34,
+			backgroundColor: "#F6163C",
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			borderRadius: 20,
+			marginRight: 8,
+		},
+		buttonMessageSmall: {
+			width: 113,
+			height: 34,
+			backgroundColor: notBackTheme,
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			borderRadius: 20,
+			marginLeft: 8,
+		},
+		linePromotions: {
+			width: "50%",
+			height: 2,
+			backgroundColor: notBackTheme,
+			alignSelf: "flex-end",
+		},
+		imagePromotion: {
+			width: width - 25,
+			height: 137,
+		},
+	});
+};
