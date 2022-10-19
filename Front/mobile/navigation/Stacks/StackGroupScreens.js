cccb480c00e22384f9bcecb5003a1dd1636d3e4a
Smael Nicolas <nico.smael@gmail.com>
2022-10-19 13:10:05 -0300

    stack groups screens

---
+++ Front/mobile/navigation/Stacks/StackGroupScreens.js
@@ -0,0 +1,41 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useContext } from "react";
+import { ThemeContext } from "../../Context/Theme";
+import { GroupsDetail } from "../screens/GroupsDetailsScreen/GroupsDetails";
+import { GroupsScreen } from "../screens/GroupsScreen/GroupsScreen";
+
+const GroupStackNavigator = createNativeStackNavigator();
+
+export const StackGroupScreens = () => {
+	const { backTheme, notBackTheme } = useContext(ThemeContext);
+
+	return (
+		<GroupStackNavigator.Navigator
+			initialRouteName='Groups'
+			screenOptions={{
+				headerBackButtonMenuEnabled: true,
+				headerBackTitleVisible: true,
+				headerTransparent: true,
+				headerTintColor: notBackTheme,
+				headerTitleAlign: "center",
+				headerStyle: {
+					backgroundColor: backTheme,
+				},
+			}}>
+			<GroupStackNavigator.Screen
+				name='Groups'
+				component={GroupsScreen}
+				options={{
+					headerShown: false,
+				}}
+			/>
+			<GroupStackNavigator.Screen
+				name='Grupo'
+				component={GroupsDetail}
+				options={{
+					headerShown: true,
+				}}
+			/>
+		</GroupStackNavigator.Navigator>
+	);
+};
