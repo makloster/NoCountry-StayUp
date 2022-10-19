2499015cfcecd81bc4d6af70f4066c6e249c35de
frangmen <frangm2702@gmail.com>
2022-10-18 23:55:38 -0500

    working on web

--- Front/web/src/pages/Home.jsx
+++ Front/web/src/pages/Home.jsx
@@ -2,10 +2,12 @@ import Presentation3 from "../components/Home/Presentation3";
 import Presentation5 from "../components/Home/Presentation5";
 import Presentation2 from "../components/Home/Presentation2";
 import Presentation1 from "../components/Home/Presentation1";
+import UploadImages from '../components/VistaCargaDeDatos/UploadImages'
 
 const Home = () => {
     return (
         <>
+            <UploadImages/>
             <Presentation1 />
             <Presentation2 />
             <Presentation3 />
