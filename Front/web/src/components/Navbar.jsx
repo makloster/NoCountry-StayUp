7c4b9a5c5d3015181935ff8a1c6a9217da950136
Javier A <javiersettex@gmail.com>
2022-10-19 14:53:10 -0500

    refactor footer

--- Front/web/src/components/Navbar.jsx
+++ Front/web/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import imgLogo from "../assets/logo.png";
 
 const Navbar = () => {
     return (
-        <div className='w-full h-[90px] bg-[#181818]  flex items-center justify-center'>
-            <div className='w-[80%] max-w-[1400px] h-full  flex items-center justify-between'>
-                <img src={imgLogo} alt='' />
-                <button className='w-[250px] h-[50px] bg-[#F6163C] text-white  rounded-[15px] flex items-center justify-center'>
+        <div className="w-full h-[90px] bg-[#181818]  flex items-center justify-center">
+            <div className="w-[80%] max-w-[1400px] h-full  flex items-center justify-between">
+                <img src={imgLogo} alt="" />
+                <button className=" hidden md:flex w-[250px] h-[50px] bg-[#F6163C] text-white  rounded-[15px]  items-center justify-center">
                     Ingresar Organizador
                 </button>
             </div>
