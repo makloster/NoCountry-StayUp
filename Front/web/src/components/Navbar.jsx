7bec4a5b229b5c30f19f82ebf011fb5eea211889
Javier A <javiersettex@gmail.com>
2022-10-20 17:52:48 -0500

    refactor p  4-5

--- Front/web/src/components/Navbar.jsx
+++ Front/web/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import imgLogo from "../assets/logo.png";
+import imgLogo from "../assets/General/LogoWhite.svg";
 
 const Navbar = () => {
     return (
         <div className="w-full h-[90px] bg-[#181818]  flex items-center justify-center">
             <div className="w-[80%] max-w-[1400px] h-full  flex items-center justify-between">
-                <img src={imgLogo} alt="" />
+                <img src={imgLogo} className="w-[100px] md:w-[160px]" alt="" />
                 <button className=" hidden md:flex w-[250px] h-[50px] bg-[#F6163C] text-white  rounded-[15px]  items-center justify-center">
                     Ingresar Organizador
                 </button>
