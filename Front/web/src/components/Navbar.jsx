bfdbcdb07ff7401a570db114a0357330b1347b82
frangmen <frangm2702@gmail.com>
2022-10-21 12:54:41 -0500

    mergeando main final

--- Front/web/src/components/Navbar.jsx
+++ Front/web/src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div className='w-full h-[90px] bg-[#181818]  flex items-center justify-center'>
-            <div className='w-[80%] max-w-[1400px] h-full  flex items-center justify-between'>
-                <Link to={"/"}>
-                    <img src={imgLogo} alt='' />
+            <div className='w-[80%] max-w-[1400px] h-full absolute flex items-center justify-between'>
+                <Link class="w-16 md:w-32 lg:w-48 " to={"/"}>
+                    <img src={imgLogo} alt='hola' />
                 </Link>
-                <Link to={'./formularioOrganizador'}>
-                <button className='w-[250px] h-[50px] bg-[#F6163C] text-white  rounded-[15px] flex items-center justify-center'>
-                    Ingresar Organizador
-                </button>
+                <Link to={"./formularioOrganizador"}>
+                    <button className='w-[250px] h-[50px] bg-[#F6163C] text-white  rounded-[15px] flex items-center justify-center'>
+                        Ingresar Organizador
+                    </button>
                 </Link>
             </div>
         </div>
--- Front/web/src/components/VistaCargaDeDatos/ComponentesFormulario/Precio.jsx
+++ Front/web/src/components/VistaCargaDeDatos/ComponentesFormulario/Precio.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Precio = () => {
     return (
         <div className='bg-white h-[120px] rounded-[15px] flex   flex-col items-center justify-center'>
-            <h1 style={{display:"initial"}}>Precio</h1>
+            <h1 style={{ display: "initial" }}>Precio</h1>
             <h2>
                 Del monto que ingreses se te cobrará una comisión de 10% que
                 figurará en su estado de cuenta en la fecha de cobro
